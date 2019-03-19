bebd20316cd4204ebd7e35a468384515f59ff6ad
alumno <alumno>
2019-03-19 12:05:04 -0300

    Primera Clase

---
+++ Linares.Ricardo/Ejercicio-01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int minimo;
+            int maximo;
+            float acumulador = 0;
+            int contador = 0;
+            float promedio;
+
+
+            Console.WriteLine("Ingrese 5 Numeros: ");
+
+            numero = int.Parse(Console.ReadLine());
+            minimo = numero;
+            maximo = numero;
+            acumulador += numero;
+            contador++;
+
+            for (int i = 0; i < 4; i++)
+            {
+                numero = int.Parse(Console.ReadLine());
+
+                if (minimo > numero)
+                    minimo = numero;
+                else if (maximo < numero)
+                    maximo = numero;
+                acumulador += numero;
+                contador++;
+            }
+
+            promedio = acumulador / contador;
+            Console.WriteLine(" Valor Maximo = {0}\n Valor Minimo = {1}\n Promedio = {2}", maximo, minimo, promedio);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Linares.Ricardo/Proyecto01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            string name;
+            Console.WriteLine("hola mundo");
+            Console.ReadKey();
+
+            Console.Write("Ingrese su nombre : ");
+
+            name = Console.ReadLine();
+
+            Console.Write("Su nombre es ");
+            WW3names(name);
+            Console.ReadLine();
+
+            Console.Write("Ingrese su apellido : ");
+
+            name = name + " " + Console.ReadLine();
+
+            Console.Write("Su nombre completo es ");
+            WW3names(name);
+            Console.ReadLine();
+        }
+
+        public static void WW3names(string objetivo)
+        {
+            ConsoleColor colorBase = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(objetivo);
+            Console.ForegroundColor = colorBase;
+            return;
+        }
+    }
+}
