6f73c5a3d896eefe60e88b3fd7b148144b3c2a37
alumno <alumno>
2019-05-15 12:01:57 -0300

    15/05

---
+++ Linares.Ricardo/Clase15_WindowsForm/FrmPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Clase15_WindowsForm
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lstVisor = new System.Windows.Forms.ListBox();
+            this.btnTraer = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(12, 28);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(510, 41);
+            this.txtValor.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(551, 11);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(152, 41);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "&Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lstVisor
+            // 
+            this.lstVisor.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstVisor.FormattingEnabled = true;
+            this.lstVisor.ItemHeight = 20;
+            this.lstVisor.Location = new System.Drawing.Point(12, 102);
+            this.lstVisor.Name = "lstVisor";
+            this.lstVisor.Size = new System.Drawing.Size(700, 284);
+            this.lstVisor.TabIndex = 2;
+            // 
+            // btnTraer
+            // 
+            this.btnTraer.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTraer.Location = new System.Drawing.Point(12, 427);
+            this.btnTraer.Name = "btnTraer";
+            this.btnTraer.Size = new System.Drawing.Size(152, 41);
+            this.btnTraer.TabIndex = 3;
+            this.btnTraer.Text = "&Traer";
+            this.btnTraer.UseVisualStyleBackColor = true;
+            this.btnTraer.Click += new System.EventHandler(this.btnTraer_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Arial Narrow", 21.75F);
+            this.btnBuscar.Location = new System.Drawing.Point(551, 58);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(152, 41);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Archivos cargados";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 480);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnTraer);
+            this.Controls.Add(this.lstVisor);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtValor);
+            this.Name = "FrmPrincipal";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ListBox lstVisor;
+        private System.Windows.Forms.Button btnTraer;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Linares.Ricardo/Clase15_WindowsForm/FrmPrincipal.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Clase15_WindowsForm
+{
+    public partial class FrmPrincipal : Form
+    {
+        private string _domain;
+        private OpenFileDialog _openFileDialog;
+        public FrmPrincipal()
+        {
+
+            InitializeComponent();
+            this._domain =  Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\NewText.txt";
+            this._openFileDialog = new OpenFileDialog();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                StreamWriter writer = new StreamWriter(this._domain, true);
+                if(this.txtValor.Text != "" && this.txtValor.Text != null)
+                {
+                    writer.WriteLine(this.txtValor.Text);
+                    if (this.lstVisor.Items.Contains(((FileStream)(writer.BaseStream)).Name) != true)
+                    {
+                        this.lstVisor.Items.Add(((FileStream)(writer.BaseStream)).Name);
+                    }
+                }
+                else
+                {
+                    writer.Close();
+                    throw new Exception("Debe llenar el textBox con texto");
+                }
+                writer.Close();
+
+            }
+            catch (Exception exception)
+            {
+
+                this.Error(exception);
+            }
+        }
+
+        private void btnTraer_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                StreamReader reader = new StreamReader((string)this.lstVisor.SelectedItem);
+                string lines = reader.ReadToEnd();
+
+                this.txtValor.Text = lines;
+                reader.Close();
+            }
+            catch (Exception exception)
+            {
+                this.Error(exception);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this._openFileDialog.DefaultExt = "*.txt";
+                this._openFileDialog.AddExtension = true;
+                this._openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                this._openFileDialog.FilterIndex = 1;
+                DialogResult n = this._openFileDialog.ShowDialog();
+                if (n == DialogResult.OK)
+                {
+                    this._domain = this._openFileDialog.FileName;
+                    if (this.lstVisor.Items.Contains(this._domain) != true)
+                        this.lstVisor.Items.Add(this._domain);
+                }
+            }
+            catch (Exception exception)
+            {
+                this.Error(exception);
+            }
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            this._openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            this._openFileDialog.Title = "Abre un archivo .Txt";
+        }
+
+        private void Error(Exception e)
+        {
+            MessageBox.Show(e.Message, "error");
+        }
+    }
+}
